e97436476385d1642f9156e3d3f51f784f922c85
Lucas Miranda <mirandalucasabel@gmail.com>
2021-09-10 18:42:55 -0300

    agrego base del proyecto

---
+++ tp1/tp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
