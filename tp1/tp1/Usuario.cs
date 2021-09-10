32178b5301486f334368025229ae37d1a5f75b43
LautaroRDavinci <lautaro.ramos@davinci.edu.ar>
2021-09-10 19:51:06 -0300

    Prueba 3

--- tp1/tp1/Usuario.cs
+++ tp1/tp1/Usuario.cs
@@ -4,11 +4,11 @@ namespace tp1
 {
     public abstract class Usuario
     {
-            public int id { get; set; }
-            public int dni { get; set; }
-            public string nombre { get; set; }
-            public string mail { get; set; }
-            public string password { get; set; }
+        public int id { get; set; };
+        public int dni { get; set; };
+        public string nombre { get; set; };
+        public string mail { get; set; };
+        public string password { get; set; };
             
     }
     
